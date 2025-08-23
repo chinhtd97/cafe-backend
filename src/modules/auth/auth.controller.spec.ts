74bc300db84a067b115a3a449e8d62f932d293fa
Litch9x <tranducchinh20021997@gmail.com>
2025-08-24 06:59:05 +0700

    fix(test): db connect

--- src/modules/auth/auth.controller.spec.ts
+++ src/modules/auth/auth.controller.spec.ts
@@ -2,17 +2,21 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { AuthController } from './auth.controller';
 import { AuthModule } from './auth.module';
 import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      imports: [AuthModule],
+      imports: [
+        MongooseModule.forRoot('mongodb://localhost:27017/cafe-shop'), // fake test DB
+        AuthModule,
+      ],
     })
       .overrideProvider(ConfigService)
       .useValue({
-        get: jest.fn().mockReturnValue('super-secret-test'), // mock secret
+        get: jest.fn().mockReturnValue('super-secret-test'),
       })
       .compile();
 
